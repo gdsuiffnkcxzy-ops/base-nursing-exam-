6b06203d1793de50e3739e7c16d74c14d7772d99
Nursing exam <gdsuiffnkcxzy@gmail.com>
2026-05-15 11:11:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // يجب أن يكون base مساوياً لاسم المستودع مع شرطتين مائلتين في البداية والنهاية
-  base: '/base-nursing-exam/',
+  base: '/base-nursing-exam-/',
 })
